2aafb1420ac0c73dc6d9ad9591dff419a7858f31
maha2126 <mariam.petrosyan1998@mail.ru>
2023-08-17 13:35:56 +0400

    counter with react using useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  //const [counter, setCounter]  = React.useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1 )
+  };
+  const onClickMinus = () => {
+    setCount(count - 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+        <h3 className='coun-name'>Counter</h3>
+        <h1 className='coun'>{count}</h1>
+        <button className='up' onClick={onClickPlus}>Up</button>
+        <button className='down' onClick={onClickMinus}>Down</button>
     </div>
   );
 }
